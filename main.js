92897c3e9ccfdf74e0f9047958afcddd4a1c198d
Brodey Linderman <blinderman52@hotmail.com>
2023-05-04 11:12:08 -0400

    Pc work (#4)
    * changed compareDNA function to work correctly
    
    * added new function, added some notes and added a new call to create and store organisms
    
    ---------

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// Factory function that creates a new organism
 const pAequorFactory = (specimanNum, dna) => {
   return {
     specimanNum,
@@ -38,12 +39,37 @@ const pAequorFactory = (specimanNum, dna) => {
      total = ((matchingBase / this.dna.length) * 100).toFixed(2);
       console.log(`${this.specimanNum} and ${dnaObj.specimanNum} have ${total}% DNA in common.`);
      },
+     willLikelySurvive(){
+        const totalMatches = this.dna.filter(base => base === 'G' || base === 'C')
+        if (((totalMatches.length / this.dna.length) * 100) >= 60 ){
+          return true;
+        }
+     },
    };
  };
 
-const ex1 = ["A", "C", "T", "G"];
+
+ /*
+ // this was all used for testing purposes to ensure functions worked properly once implemented
+const ex1 = ["C", "C", "A", "G"];
 const ex2 = ["C", "A", "T", "T"];
 
 
 pAequorFactory(1, mockUpStrand()).mutate();
 pAequorFactory(1, ex1).compareDNA(pAequorFactory(2, ex2));
+pAequorFactory(1, ex1).willLikelySurvive();
+*/
+
+// Creating a simulation that creates 30 instances of pAequor that can survive in their natural env and store them into an array
+let newStrands = [];
+let strandCounter = 0;
+
+while(newStrands.length < 30){
+  let currentOrg = pAequorFactory(strandCounter, mockUpStrand())
+  if (currentOrg.willLikelySurvive() === true){
+    newStrands.push(`Speciman Number: ${currentOrg.specimanNum}, DNA Strand: ${currentOrg.dna}`);
+  };
+  strandCounter++;
+};
+
+console.log(newStrands);
